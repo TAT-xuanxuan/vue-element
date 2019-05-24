fa6279f3e146201e039689bc80fd227816925755
cool <wangxy0436@foxmail.com>
2019-05-24 16:01:33 +0800

    上传项目

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const config = {
+    entry: path.join(__dirname, "src/main.js"),
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, "dist/js")
+    },
+    mode:"none",
+    module: {
+        rules: [{
+                test: /.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
+
+
+module.exports = config
